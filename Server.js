bfc8f230882993177892c8cc6d8dfe6a8e039452
Pratik Dubey <146483524+Pratikdubey7735@users.noreply.github.com>
2024-06-09 06:35:27 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,27 +2,34 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-const app = express();
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
+
 dotenv.config({
-    path: './.env'
-})
+  path: './.env'
+});
+
+const app = express();
 const PORT = process.env.PORT || 8000;
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cors({
+  origin: "https://upstepacademy.vercel.app"
+}));
 
-  origin:"https://upstepacademy.vercel.app/"
-}
-));
-
+// MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI);
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
   console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
 });
+
+// Mongoose Schemas and Models
 const demoSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -40,16 +47,19 @@ const gameResultSchema = new mongoose.Schema({
 });
 
 const GameResultModel = mongoose.model('GameResult', gameResultSchema);
-app.get('/',async(req,res)=>{
-  res.send("Welcome to the world of chess ")
-})
+
+// Routes
+app.get('/', (req, res) => {
+  res.send("Welcome to the world of chess");
+});
+
 app.post('/api/submitFormData', async (req, res) => {
   try {
     const formData = req.body;
     const demoInstance = new DemoModel(formData);
     await demoInstance.save();
 
-    res.status(201).json({ message:'Form data saved successfully' });
+    res.status(201).json({ message: 'Form data saved successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
@@ -61,15 +71,12 @@ app.post('/api/saveGameResult', async (req, res) => {
     const { playerNames, winner } = req.body;
     const gameResultInstance = new GameResultModel({ playerNames, winner });
     await gameResultInstance.save();
+
     res.status(201).json({ message: 'Game result saved successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get('/api/getLeaderboard', async (req, res) => {
@@ -81,3 +88,9 @@ app.get('/api/getLeaderboard', async (req, res) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
